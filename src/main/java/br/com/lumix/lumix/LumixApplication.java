30765656e7768ac6d51b5f5b68eb24d2b1315f69
jgabrielv2 <jgabrielv2@outlook.com>
2024-02-20 15:24:42 -0300

    commit inicial

---
+++ src/main/java/br/com/lumix/lumix/LumixApplication.java
@@ -0,0 +1,13 @@
+package br.com.lumix.lumix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumixApplication.class, args);
+	}
+
+}
