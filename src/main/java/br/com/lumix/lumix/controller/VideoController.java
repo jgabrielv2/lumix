b64df251581c6daa3a001143b095b9f4865e212e
jgabrielv2 <jgabrielv2@outlook.com>
2024-02-21 15:12:41 -0300

    Atender uma requisição GET que exiba todos os vídeos com o seguinte endpoint: /videos

--- src/main/java/br/com/lumix/lumix/controller/VideoController.java
+++ src/main/java/br/com/lumix/lumix/controller/VideoController.java
@@ -2,18 +2,16 @@ package br.com.lumix.lumix.controller;
 
 import br.com.lumix.lumix.dto.create.DadosCriacaoVideo;
 import br.com.lumix.lumix.dto.read.DadosListagemVideo;
+import br.com.lumix.lumix.dto.update.DadosAtualizacaoVideo;
 import br.com.lumix.lumix.service.VideoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("videos")
+@RequestMapping("/videos")
 public class VideoController {
 
     private final VideoService videoService;
@@ -30,8 +28,29 @@ public class VideoController {
         return ResponseEntity.created(uri).body(video);
     }
 
+    @GetMapping
     public ResponseEntity<List<DadosListagemVideo>> listarTodos() {
-        List<DadosListagemVideo> videos = videoService.findAll();
+        var videos = videoService.findAll();
         return ResponseEntity.ok(videos);
     }
+
+
+    @GetMapping("{/id}")
+    public ResponseEntity<DadosListagemVideo> listarPorId(@PathVariable Long id) {
+        var video = videoService.findById(id);
+        return ResponseEntity.ok(video);
+    }
+
+    @PutMapping("{/id}")
+    public ResponseEntity<DadosListagemVideo> atualizar(@PathVariable Long id, @RequestBody DadosAtualizacaoVideo dadosAtualizacaoVideo) {
+        var video = videoService.update(id, dadosAtualizacaoVideo);
+        return ResponseEntity.ok(video);
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+        videoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
